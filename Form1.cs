21ded710643b5d48ddb7a99e6334d6325a2144a6
Pioziomgames <73062121+Pioziomgames@users.noreply.github.com>
2021-06-29 18:32:49 +0200

    Finnished very basic functionality, needs only deleting of old parts to fully work

--- Form1.cs
+++ Form1.cs
@@ -9,8 +9,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using DarkUI.Forms;
-
-
+using AtlusFileSystemLibrary;
+using AtlusFileSystemLibrary.FileSystems.PAK;
+using AtlusFileSystemLibrary.Common.IO;
+using AmicitiaLibrary.FileSystems.AMD;
+using System.Diagnostics;
+using System.Reflection;
 
 
 namespace Pomidor
@@ -29,9 +33,10 @@ namespace Pomidor
 
         private void darkButton1_Click(object sender, EventArgs e)
         {
+            string[] files;
             try
             {
-                string[] files = Directory.GetFiles(pathTextBox.Text, "*.amd", SearchOption.AllDirectories);
+                files = Directory.GetFiles(pathTextBox.Text, "*.amd", SearchOption.AllDirectories);
                 AllFIlesProgressBar.Value = 0;
                 CurrentFileProgressBar.Value = 0;
                 AllFIlesProgressBar.Maximum = files.Length;
@@ -46,12 +51,13 @@ namespace Pomidor
                 MessageBox.Show("Error while searching for amd files");
                 return;
             }
+            string NewParts = "";
+            string NewBones = "";
+            string NewMaterials = "";
+            string NewTextures = "";
             if (File.Exists(newmdstextbox.Text))
             {
-                string NewParts = "";
-                string NewBones = "";
-                string NewMaterials = "";
-                string NewTextures = "";
+                
                 try
                 {
                     string line;
@@ -99,13 +105,13 @@ namespace Pomidor
                                 savetexture = true;
                             }
                             if (savepart)
-                                NewParts += line;
+                                NewParts += line + "\n";
                             if (savebone)
-                                NewBones += line;
+                                NewBones += line + "\n";
                             if (savetexture)
-                                NewTextures += line;
+                                NewTextures += line + "\n";
                             if (savematerial)
-                                NewMaterials += line;
+                                NewMaterials += line + "\n";
                         }
                     }
                 }
@@ -120,9 +126,233 @@ namespace Pomidor
                 MessageBox.Show("Mds file doesn't exists");
                 return;
             }
-            //Here is where the pain begins
+            foreach (string file in files)
+            {
+                //extract gmo data from amd files
+                bool extracted = false;
+                AmdFile amdfile = new AmdFile(file);
+                string GMOpath = "";
+                foreach (var chunk in amdfile.Chunks)
+                {
+                    if (chunk.Tag.ToUpper().Equals("MODEL_DATA"))
+                    {
+                        GMOpath = Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + ".gmo";
+                        File.WriteAllBytes(GMOpath, chunk.Data);
+                        
+                    }
+                    if (File.Exists(GMOpath))
+                        extracted = true;
+                }
+                if (!extracted)
+                {
+                    Debug.WriteLine("Couldn't find model data");
+                    //hopefully never happends
+                }
+                //convert gmo to mds
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Dependencies\\GMOTool\\GMOTool.exe";
+                cmd.StartInfo.Arguments = $"\"{GMOpath}\"";
+                cmd.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                cmd.StartInfo.Arguments += " -E";
+                cmd.Start();
+                cmd.WaitForExit();
+                //extract textures from gmo 
+                ExtractTextures(GMOpath);
+                //edit mds
+                string MDSpath = Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + ".mds";
+
+                string FullFile = System.IO.File.ReadAllText(MDSpath);
+
+                string NoMotion = FullFile.Remove(FullFile.IndexOf("\tMotion \""));
+                string OnlyMotion = FullFile.Substring(FullFile.IndexOf("\tMotion \""));
+                string EditedMDS = "";
+                using (System.IO.StringReader reader = new System.IO.StringReader(NoMotion))
+                {
+                    string line;
+                    bool saveline = true;
+                    int counter = 0;
+                    bool bonesAdded = false;
+                    bool partsAdded = false;
+                    bool materialsAdded = false;
+                    bool texturesAdded = false;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        counter++;
+
+                        if (line.StartsWith("\tBone \"") && counter > 10 && bonesAdded == false)
+                        {
+                            EditedMDS += NewBones + "\n";
+                            bonesAdded = true;
+                        }
+
+                        if (line.StartsWith("\tPart \"") && partsAdded == false)
+                        {
+                            EditedMDS += NewParts + "\n";
+                            partsAdded = true;
+                        }
+
+                        if (line.StartsWith("\tTexture \"") && texturesAdded == false)
+                        {
+                            EditedMDS += NewTextures + "\n";
+                            texturesAdded = true;
+                        }
+
+                        if (line.StartsWith("\tMaterial \"") && materialsAdded == false)
+                        {
+                            EditedMDS += NewMaterials + "\n";
+                            materialsAdded = true;
+                        }
+
+                        if (saveline)
+                            EditedMDS += line + "\n" ;
+                    }
+                }
+
+                //overwrite mds
+                using (StreamWriter writetext = new StreamWriter(MDSpath))
+                {
+                    writetext.Write(EditedMDS + OnlyMotion);
+                }
+                //convert to gmo
+                Process cmd3 = new Process();
+                cmd3.StartInfo.FileName = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Dependencies\\GMOTool\\GMOTool.exe";
+                cmd3.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                cmd3.StartInfo.Arguments = $"\"{MDSpath}\"";
+                cmd3.StartInfo.Arguments += " -PSV";
+                cmd3.Start();
+                cmd3.WaitForExit();
+                System.Threading.Thread.Sleep(1000);
+
+                // run tge's tool to fix gmo
+                Process cmd4 = new Process();
+                cmd4.StartInfo.FileName = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\Dependencies\\p4gpc-gmofix\\p4gpc-gmofix.exe";
+                cmd4.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                cmd4.StartInfo.Arguments = $"\"{GMOpath}\"";
+                cmd4.Start();
+                cmd4.WaitForExit();
+
+
+                //convert gmo back to amd
+                string AMDpath = Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) + ".amd";
+                if (File.Exists(AMDpath))
+                {
+                    AmdFile amd = new AmdFile(AMDpath);
+                    foreach (var chunk in amd.Chunks)
+                    {
+                        if (chunk.Tag.ToUpper().Equals("MODEL_DATA"))
+                        {
+                            chunk.Data = File.ReadAllBytes(GMOpath);
+                        }
+                    }
+                    amd.Save(AMDpath);
+                }
+                else
+                {
+                    AmdFile amd = new AmdFile();
+                    AmdChunk chunk = new AmdChunk() { Data = File.ReadAllBytes(GMOpath), Flags = 257, Tag = "MODEL_DATA" };
+                    amd.Chunks.Add(chunk);
+                    amd.Save(AMDpath);
+                }
+            }
+            
         }
 
+
+        //stolen code from shirnefox's converter
+        private void ExtractTextures(string path)
+        {
+            //Get texture names
+            List<string> textureNames = new List<string>();
+            byte[] fileBytes = File.ReadAllBytes(path);
+            int offset = 0;
+
+            for (int i = 0; i < fileBytes.Count(); i++)
+            {
+                try
+                {
+                    offset = FindSequence(File.ReadAllBytes(path), i, Encoding.ASCII.GetBytes(".tm2"));
+                    i = offset;
+                    using (EndianBinaryReader reader = new EndianBinaryReader(File.OpenRead(path), Endianness.BigEndian))
+                    {
+                        int dim = 40;
+                        if (i <= dim) dim = 20;
+                        reader.BaseStream.Position = i - dim;
+                        byte[] nameBytes = reader.ReadBytes(dim + 4);
+                        string name = Encoding.UTF8.GetString(nameBytes);
+                        textureNames.Add(name.Substring(name.LastIndexOf('\0') + 1));
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            if (textureNames.Count > 0)
+            {
+                if (File.Exists($"{Path.GetDirectoryName(path)}\\textures\\{textureNames[0]}"))
+                    return;
+
+                //Get textures and write files
+                offset = 0;
+                for (int i = 0; i < textureNames.Count(); i++)
+                {
+                    try
+                    {
+                        offset = FindSequence(File.ReadAllBytes(path), offset, Encoding.ASCII.GetBytes("TIM2"));
+                        if (!Directory.Exists($"{Path.GetDirectoryName(path)}\\textures"))
+                            Directory.CreateDirectory($"{Path.GetDirectoryName(path)}\\textures");
+                        if (File.Exists($"{Path.GetDirectoryName(path)}\\textures\\{textureNames[i]}"))
+                            File.Delete($"{Path.GetDirectoryName(path)}\\textures\\{textureNames[i]}");
+                        string newFile = $"{Path.GetDirectoryName(path)}\\textures\\{textureNames[i]}";
+                        using (EndianBinaryReader reader = new EndianBinaryReader(File.OpenRead(path), Endianness.BigEndian))
+                        {
+                            reader.BaseStream.Position = offset;
+                            reader.ReadBytes(16);
+                            int size = reader.ReadInt32() + 16;
+
+                            reader.BaseStream.Position = offset;
+                            File.WriteAllBytes(newFile, reader.ReadBytes(size));
+                            offset++;
+                        }
+                    }
+                    catch
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        //shrinefox's code again
+        private static int FindSequence(byte[] array, int start, byte[] sequence)
+        {
+            int end = array.Length - sequence.Length; // past here no match is possible
+            byte firstByte = sequence[0]; // cached to tell compiler there's no aliasing
+
+            while (start < end)
+            {
+                // scan for first byte only. compiler-friendly.
+                if (array[start] == firstByte)
+                {
+                    // scan for rest of sequence
+                    for (int offset = 1; offset < sequence.Length; ++offset)
+                    {
+                        if (array[start + offset] != sequence[offset])
+                        {
+                            break; // mismatch? continue scanning with next byte
+                        }
+                        else if (offset == sequence.Length - 1)
+                        {
+                            return start; // all bytes matched!
+                        }
+                    }
+                }
+                ++start;
+            }
+
+            // end of array reached without match
+            return -1;
+        }
         private void darkLabel4_Click(object sender, EventArgs e)
         {
 
